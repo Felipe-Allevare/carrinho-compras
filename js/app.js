f73869e9af69390cc11e2e8c6b3222f821abcb86
Felipe Alievi <FELIPEALIEVI1@GMAIL.COM>
2025-08-15 08:41:35 -0300

    sec commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade');
+
+
+}
+function limpar() {
+
+}
